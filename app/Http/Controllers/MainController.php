c661e4cfe51f93a46f1863ff2072acb10e5853bb
Ewa Podolak <ewa.podolak@parall.ax>
2023-06-14 10:07:31 +0100

    remove example files as not written yet

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -9,12 +9,5 @@ class MainController extends Controller
     function ReturnToDoListView(Request $request)
     {
         return view('ToDoList', ['name'=>'Your Name']);
-
-        //Example Code
-        //return view('ToDoListExample', ['name'=>'Your Name']);
-        //Eloquent getting all things from database
-        //ModelName::get();
-        //Eloquent getting things with where clause
-        //ModelName::where('name', 'Random Name')->get();
     }
 }
